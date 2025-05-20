2449c00f4101f31e6859096e3ac1be4eb039c156
Julian Lenz <j.lenz@hzdr.de>
2025-05-20 08:56:11 +0200

    Add end-to-end test for free-formula density

---
+++ test/python/picongpu/end-to-end/compare_particles.py
@@ -0,0 +1,64 @@
+"""
+This file is part of PIConGPU.
+Copyright 2025 PIConGPU contributors
+Authors: Julian Lenz
+License: GPLv3+
+"""
+
+import sys
+import openpmd_api as opmd
+import numpy as np
+import pandas as pd
+
+
+def read_particles(series_name):
+    series = opmd.Series(str(series_name), opmd.Access.read_only)
+    names, particles = zip(*series.iterations[0].particles.items())
+
+    data = pd.concat((particle.to_df() for particle in particles), keys=names)
+    return data.assign(
+        setup=data.index.to_frame()[0].apply(lambda x: x.split("_")[0]),
+        impl=data.index.to_frame()[0].apply(lambda x: "_".join(x.split("_")[1:])),
+    ).set_index(["setup", "impl"], drop=True)
+
+
+def sort_particles(data):
+    return data.groupby(["setup", "impl"]).apply(lambda df: df.sort_values(list(df.columns), axis=0))
+
+
+def compare_particles_per_setup(data):
+    result = True
+    for setup_name, df in sort_particles(data).groupby("setup"):
+        # We're doing (more than) twice the work here but that's fine:
+        for lhs_name, lhs_df in df.groupby("impl"):
+            for rhs_name, rhs_df in df.groupby("impl"):
+                matched = np.allclose(lhs_df.reset_index(drop=True), rhs_df.reset_index(drop=True))
+                if not matched:
+                    print(f"Mismatch found in {setup_name}: {lhs_name} != {rhs_name}")
+                result *= matched
+    return result
+
+
+def compute_densities_per_setup_and_impl(data):
+    return data.groupby(["setup", "impl", "positionOffset_x", "positionOffset_y", "positionOffset_z"])[
+        "weighting"
+    ].sum()
+
+
+def compute_densities_from_particles(series_name):
+    return compute_densities_per_setup_and_impl(read_particles(series_name))
+
+
+def compare_particles(series_name):
+    """
+    Compare particles from the given series and return True if they all compare equal.
+    """
+    return compare_particles_per_setup(read_particles(series_name))
+
+
+def main(series_name):
+    compare_particles(series_name)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
---
+++ test/python/picongpu/end-to-end/free_formula_density.py
@@ -0,0 +1,252 @@
+"""
+This file is part of PIConGPU.
+Copyright 2025 PIConGPU contributors
+Authors: Julian Lenz
+License: GPLv3+
+"""
+
+import unittest
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import sympy
+from .compare_particles import compare_particles, compute_densities_from_particles
+from picongpu import picmi
+from picongpu.picmi.diagnostics.timestepspec import TimeStepSpec
+
+
+# This is just meant as a form of namespace to bundle the setups together.
+class Gaussian:
+    def __init__(self):
+        self.parameters = {
+            "density": 1.0e25,
+            # cell size is 1, so we don't need to distinguish
+            # between number of cells and value in SI
+            "vacuum_cells_front": 14,
+            "center_front": 29,
+            "center_rear": 54,
+            "sigma_front": 10,
+            "sigma_rear": 3,
+            "power": 2.0,
+            "factor": -2.0,
+        }
+        self.distributions = {
+            "predefined": picmi.GaussianDistribution(**self.parameters),
+            "free_form": picmi.AnalyticDistribution(
+                lambda x, y, z: self.free_form(y, cell_size_y=1.0, **self.parameters)
+            ),
+        }
+
+    @staticmethod
+    def free_form(
+        y,
+        density,
+        cell_size_y,
+        vacuum_cells_front,
+        center_front,
+        center_rear,
+        sigma_front,
+        sigma_rear,
+        power,
+        factor,
+    ):
+        # apparently, our SI position is the centre of the cell
+        y += -0.5
+        # The last term undoes the shift to the cell origin.
+        vacuum_y = vacuum_cells_front * cell_size_y - 0.5
+
+        exponent = sympy.Piecewise(
+            (sympy.Abs((y - center_front) / sigma_front), y < center_front),
+            (sympy.Abs((y - center_rear) / sigma_rear), y >= center_rear),
+            (0.0, True),
+        )
+        return sympy.Piecewise((0.0, y < vacuum_y), (density * sympy.exp(factor * exponent**power), True))
+
+
+# This is just meant as a form of namespace to bundle the setups together.
+class Uniform:
+    def __init__(self):
+        self.arbitrary_value = 8.0e24
+        self.distributions = {
+            "predefined": picmi.UniformDistribution(density=self.arbitrary_value),
+            "free_form": picmi.AnalyticDistribution(lambda x, y, z: self.arbitrary_value),
+        }
+
+
+class Foil:
+    def __init__(self):
+        self.parameters = dict(
+            density=8.0e24,
+            front=10,
+            thickness=17,
+            exponential_pre_plasma_length=12,
+            exponential_pre_plasma_cutoff=14,
+            exponential_post_plasma_length=5,
+            exponential_post_plasma_cutoff=16,
+        )
+        self.distributions = {
+            "predefined": picmi.FoilDistribution(**self.parameters),
+            "free_form": picmi.AnalyticDistribution(lambda x, y, z: self.free_form(y, **self.parameters)),
+        }
+
+    @staticmethod
+    def free_form(
+        y,
+        density,
+        front,
+        thickness,
+        exponential_pre_plasma_length,
+        exponential_pre_plasma_cutoff,
+        exponential_post_plasma_length,
+        exponential_post_plasma_cutoff,
+    ):
+        pre_plasma_ramp = (
+            # expression
+            sympy.exp((y - front) / exponential_pre_plasma_length)
+            if exponential_pre_plasma_length is not None
+            else 0.0,
+            # condition
+            sympy.And(y < front, y > front - exponential_pre_plasma_cutoff),
+        )
+
+        post_plasma_ramp = (
+            # expression
+            sympy.exp((front + thickness - y) / exponential_post_plasma_length)
+            if exponential_post_plasma_length is not None
+            else 0.0,
+            # condition
+            sympy.And(y > front + thickness, y < front + thickness + exponential_post_plasma_cutoff),
+        )
+
+        foil = (1.0, sympy.And(y >= front, y <= front + thickness))
+        vacuum = (0.0, True)
+
+        return density * sympy.Piecewise(pre_plasma_ramp, foil, post_plasma_ramp, vacuum)
+
+
+# This is a predefined setup within PIConGPU but not PICMI.
+class LinearExponential:
+    def __init__(self):
+        self.parameters = dict(density=8.0e24, vacuum_y=14.0, gas_a=10.0, gas_b=12.0, gas_d=17.0, gas_y_max=25.0)
+        self.distributions = {
+            "free_form": picmi.AnalyticDistribution(lambda x, y, z: self.free_form(y, **self.parameters)),
+        }
+
+    @staticmethod
+    def free_form(y, density, vacuum_y, gas_a, gas_b, gas_d, gas_y_max):
+        # move to the origin of the cell
+        y += -0.5
+
+        vacuum = (0.0, y < vacuum_y)
+        linear_slope = (sympy.Max(0.0, gas_a * y + gas_b), sympy.And(y >= vacuum_y, y < gas_y_max))
+        exponential_slope = (sympy.exp(gas_d * (y - gas_y_max)), True)
+
+        return density * sympy.Piecewise(vacuum, linear_slope, exponential_slope)
+
+
+# This is a predefined setup within PIConGPU but not PICMI.
+class SphereFlanks:
+    def __init__(self):
+        self.parameters = dict(density=8.0e24, vacuum_y=14.0, center=[10.0, 40.0, 35.0], r=20.0, ri=10.0, exponent=1.0)
+        self.distributions = {
+            "free_form": picmi.AnalyticDistribution(lambda x, y, z: self.free_form(x, y, z, **self.parameters)),
+        }
+
+    @staticmethod
+    def free_form(x, y, z, density, vacuum_y, center, r, ri, exponent):
+        # move to the origin of the cell
+        y += -0.5
+
+        distance = sympy.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2 + (z - center[2]) ** 2)
+        front_vacuum = (0.0, y < vacuum_y)
+        inner_vacuum = (0.0, distance < ri)
+        sphere = (1.0, distance <= r)
+        flanks = (sympy.exp(exponent * (r - distance)), True)
+
+        return density * sympy.Piecewise(front_vacuum, inner_vacuum, sphere, flanks)
+
+
+DISTRIBUTIONS = {
+    "Uniform": Uniform().distributions,
+    "Gaussian": Gaussian().distributions,
+    "Foil": Foil().distributions,
+    "LinearExponential": LinearExponential().distributions,
+    "SphereFlanks": SphereFlanks().distributions,
+}
+
+
+def generate_name(name, suffix):
+    return name + "_" + suffix
+
+
+def add(sim, name, **distributions):
+    random_layout = picmi.GriddedLayout(n_macroparticles_per_cell=2)
+    for suffix, distribution in distributions.items():
+        species_hydrogen = picmi.Species(
+            name=generate_name(name, suffix),
+            particle_type="H",
+            initial_distribution=distribution,
+            picongpu_fixed_charge=True,
+        )
+        sim.add_species(species_hydrogen, random_layout)
+
+
+def setup_sim():
+    grid = picmi.Cartesian3DGrid(
+        number_of_cells=[64, 64, 64],
+        lower_bound=[0, 0, 0],
+        upper_bound=[64.0, 64.0, 64.0],
+        lower_boundary_conditions=["open", "open", "open"],
+        upper_boundary_conditions=["open", "open", "open"],
+    )
+    solver = picmi.ElectromagneticSolver(method="Yee", grid=grid, cfl=1.0)
+    sim = picmi.Simulation(max_steps=0, solver=solver)
+    sim.diagnostics = [picmi.diagnostics.Checkpoint(TimeStepSpec[:])]
+
+    for name, distributions in DISTRIBUTIONS.items():
+        add(sim, name, **distributions)
+
+    sim.step(0)
+    return sim
+
+
+# only run this once, so we don't compile each and every time
+SIM = None
+
+
+class TestFreeFormulaDensity(unittest.TestCase):
+    def setUp(self):
+        global SIM
+        if SIM is None:
+            SIM = setup_sim()
+        self.sim = SIM
+
+    @property
+    def result_path(self):
+        return Path(self.sim._Simulation__runner.run_dir) / "simOutput" / "checkpoints" / "checkpoint_000000.bp5"
+
+    def test_compare_particles_pairwise(self):
+        self.assertTrue(compare_particles(self.result_path))
+
+    def test_compare_particles_against_call_operator(self):
+        densities = compute_densities_from_particles(self.result_path).to_frame().rename({"weighting": "found"}, axis=1)
+        densities["expected"] = (
+            densities.reset_index(drop=False)
+            .groupby(["setup", "impl"])
+            .apply(
+                lambda df: pd.Series(
+                    # We add 0.5 because the density is evaluated at the centre of the cell.
+                    DISTRIBUTIONS[df.name[0]][df.name[1]](
+                        df.positionOffset_x + 0.5, df.positionOffset_y + 0.5, df.positionOffset_z + 0.5
+                    ),
+                    index=df.set_index(["positionOffset_x", "positionOffset_y", "positionOffset_z"], drop=True).index,
+                ).astype(float),
+                include_groups=False,
+            )
+        )
+        self.assertTrue(np.allclose(densities.found, densities.expected))
+
+
+if __name__ == "__main__":
+    unittest.main()
