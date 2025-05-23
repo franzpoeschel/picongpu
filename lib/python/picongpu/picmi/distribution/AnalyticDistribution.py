3f5024bd10f7fb35a000030e15c91ce8c97b2d24
Julian Lenz <j.lenz@hzdr.de>
2025-05-23 16:46:38 +0200

    Add documentation to AnalyticDistribution

--- lib/python/picongpu/picmi/distribution/AnalyticDistribution.py
+++ lib/python/picongpu/picmi/distribution/AnalyticDistribution.py
@@ -39,7 +39,80 @@ this method returns None.
 
 @typeguard.typechecked
 class AnalyticDistribution:
-    """Analytic Particle Distribution as defined by PICMI @todo"""
+    """
+    This class represents a plasma with a density defined by an analytic expression.
+
+    The function must be constructed using sympy functions
+    to enable code generation and manipulation.
+    This is a slight deviation from the PICMI standard.
+    Furthermore, we don't implement substitution of variables
+    as suggested in the PICMI standard.
+    Instead we propose that you write your function
+    with further variables as keyword arguments
+    and substitute them yourself before handing it over to AnalyticDistribution.
+    See the end-to-end tests for examples of this.
+
+    Writing such functions (or rather writing sympy in general)
+    comes with a few pitfalls but also advantages as listed below.
+    Make sure that you familiarise yourself with writing sympy.
+
+    Advantages:
+    - The sympy language is closer to mathematical language than to coding
+      which might make it more natural to use for some physicists.
+    - You can extract the exact distribution
+      that was used from the member `density_expression`
+      and use it any way you'd use any sympy expression.
+      In particular, you can print it to various formats,
+      say, LaTeX for automated inclusion in papers.
+    - We can easily evaluate it from within python.
+      This is what the __call__ operator does.
+      However, code generation here can have some difficulties
+      with advanced broadcasting for numpy variables.
+      The operator implements a fallback in such cases.
+      This fallback might be slightly slower on large inputs.
+      Try rewriting your function, in case you experience performance problems.
+
+    Pitfalls:
+    - We don't handle vectors yet.
+      But that's probably not too important for density expressions.
+      Just be explicit handling multiple vector components for now.
+    - Some operations might compile to suboptimal code
+      concerning numerical performance and stability.
+      Experts might want to inspect the generated C++ code
+      and/or check the unit tests for the PMAccPrinter
+      to find the precise mapping of sympy expressions
+      to PMAcc code.
+      Please approach us if you should stumble across this.
+    - Control flow is an interesting topic in this regard.
+      With respect to your three position coordinates
+      (which will be sympy symbols internally),
+      you must use pure sympy, e.g.,
+      replacing if-conditions with sympy.Piecewise and so on.
+      With respect to further parameters,
+      you're free to use any python construct you want
+      (if-conditions, loops, etc.).
+    - sympy.Piecewise has the potentially surprising property
+      that any pieces that you leave undefined are interpreted as nan.
+      This implies that adding two complementary sympy.Piecewise
+      renders the whole expression nan and not -- as you might expect --
+      defined on the union of the defined regions.
+      There are two options to circumvent this:
+      Either you can define multiple sympy.Piecewise with
+      (0.0, True) as the last condition which means 0 everywhere else.
+      (Make sure it's the last!)
+      Summing those up, works just as you'd expect.
+      Alternatively, you can define only the (expression, condition) tuples
+      and assemble them in a sympy.Piecewise in one go.
+      That's the way chosen in the end-to-end tests.
+
+    Parameters:
+        density_expression (function):
+            A Python function that takes x, y, z coordinates (in SI units)
+            and returns the density (in SI units) at that point.
+        directed_velocity (3-tuple of float):
+            A collective velocity for the particle distribution.
+            (currently untested)
+    """
 
     def __init__(self, density_expression, directed_velocity=(0.0, 0.0, 0.0)):
         self.density_function = density_expression
