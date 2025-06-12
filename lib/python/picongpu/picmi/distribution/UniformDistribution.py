Fix broadcasting in distribution __call__ operators

lib/python/picongpu/picmi/distribution/UniformDistribution.py
@@ -7,6 +7,7 @@ License: GPLv3+
 
 from ...pypicongpu import species
 from ...pypicongpu import util
+import numpy as np
 
 import picmistandard
 
@@ -71,4 +72,4 @@ class UniformDistribution(picmistandard.PICMI_UniformDistribution):
         return drift
 
     def __call__(self, x, y, z):
-        return 0.0 * (x + y + z) + self.density
+        return np.ones(np.broadcast_shapes(x, y, z)) * self.density