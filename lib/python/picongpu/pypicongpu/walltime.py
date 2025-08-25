625987646c0fd3efdc129e619b3d03c3fce5837a
Richard Pausch <r.pausch@hzdr.de>
2025-08-25 14:37:41 +0200

    fix and improve time calculation as suggested by @chillenzer

--- lib/python/picongpu/pypicongpu/walltime.py
+++ lib/python/picongpu/pypicongpu/walltime.py
@@ -15,16 +15,16 @@ class Walltime(RenderedObject, pydantic.BaseModel):
     walltime: datetime.timedelta
     """time after which the cluster scheduler will stop the simulation"""
 
+    HOUR = datetime.timedelta(hours=1.0)
+    MINUTE = datetime.timedelta(minutes=1.0)
+    SECOND = datetime.timedelta(seconds=1.0)
+
     def check(self) -> None:
         if self.walltime.total_seconds() <= 0.0:
             raise ValueError("walltime must be > 0.")
 
     def _get_serialized(self) -> dict:
-        return {
-            "walltime": "{:d}:{:02d}:{:02d}".format(
-                int(self.walltime.total_seconds() // 3600),  # hours
-                int(self.walltime.total_seconds() % 3600 // 60),  # minutes
-                int(self.walltime.total_seconds() % 3600),  # seconds
-            )
-        }  # @todo: might be cluster specific
-        # @todo might be better to use a version of __str__()
+        hours, rest = divmod(self.walltime, Walltime.HOUR)
+        minutes, rest = divmod(rest, Walltime.MINUTE)
+        seconds, _ = divmod(rest, Walltime.SECOND)
+        return {"walltime": "{:d}:{:02d}:{:02d}".format(hours, minutes, seconds)}  # @todo: might be cluster specific
