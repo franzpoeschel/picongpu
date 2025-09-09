ddeefc63436dd3398239429d87afa1ef1a9249fb
Julian Lenz <j.lenz@hzdr.de>
2025-09-09 10:02:24 +0200

    Fix test discovery

--- test/python/picongpu/__init__.py
+++ test/python/picongpu/__init__.py
@@ -1,3 +1,6 @@
-# flake8: noqa
-from .quick import *
-from .compiling import *
+from . import quick, compiling
+
+
+def load_tests(loader, standard_tests, pattern):
+    standard_tests.addTests((loader.loadTestsFromModule(module, pattern=pattern) for module in (quick, compiling)))
+    return standard_tests
