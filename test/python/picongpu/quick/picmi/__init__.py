e0bb7a0434334d77e55fb6a9c5ff37108b5c63e1
Julian Lenz <j.lenz@hzdr.de>
2025-12-11 11:35:06 +0100

    Remove picmi.Species tests (fix)

--- test/python/picongpu/quick/picmi/__init__.py
+++ test/python/picongpu/quick/picmi/__init__.py
@@ -1,6 +1,5 @@
 # flake8: noqa
 from .simulation import *  # pyflakes.ignore
-from .species import *  # pyflakes.ignore
 from .layout import *  # pyflakes.ignore
 from .distribution import *  # pyflakes.ignore
 from .gaussian_laser import *  # pyflakes.ignore
