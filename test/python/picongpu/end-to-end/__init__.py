fbe3fb0ebd6296f0f0389aafee7eadfe75029074
Julian Lenz <j.lenz@hzdr.de>
2025-12-15 21:34:46 +0100

    Remove temporary new_species test

--- test/python/picongpu/end-to-end/__init__.py
+++ test/python/picongpu/end-to-end/__init__.py
@@ -2,4 +2,3 @@
 from .free_formula_density import *  # pyflakes.ignore
 from .compare_particles import *  # pyflakes.ignore
 from .diagnostics import *  # pyflakes.ignore
-from .new_species import *  # pyflakes.ignore
